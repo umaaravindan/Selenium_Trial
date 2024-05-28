d1cf37c54d4bae273daf732673f004ae43c204fa
umaaravindan <106900252+umaaravindan@users.noreply.github.com>
2024-05-29 01:10:47 +0530

    Update AmazonLogin.py

--- AmazonLogin.py
+++ AmazonLogin.py
@@ -4,9 +4,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
+import os
 
 df = pd.read_csv('Input.csv')
+current_directory = os.getcwd()
 
+# Create a directory named "screenshots" if it doesn't exist
+screenshots_directory = os.path.join(current_directory, "screenshots")
+if not os.path.exists(screenshots_directory):
+    os.makedirs(screenshots_directory)
 # Initialize Chrome WebDriver
 driver = webdriver.Chrome()
 
@@ -17,11 +23,14 @@ searchText = driver.find_element(By.ID,"twotabsearchtextbox")
 searchText.send_keys("USB")
 searchText.send_keys(Keys.ENTER)
  
-for link in df['NextLink']: 
+for i,link in enumerate(df['NextLink']): 
  #USBMenu=driver.find_element(By.PARTIAL_LINK_TEXT,"See more")
  #USBMenu.click()
   USBOptions = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.PARTIAL_LINK_TEXT,link)))
   driver.find_element(By.PARTIAL_LINK_TEXT,link)
   USBOptions.click()
+  
+  screenshot_filename = os.path.join(current_directory, f"Screenshot_{i}.png")
+  driver.save_screenshot(screenshot_filename) 
   
 driver.quit()
